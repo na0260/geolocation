aaef2b3e9ee2a33693ec7bed128109652fdfb39d
na0260 <samjr.008@gmail.com>
2023-08-11 00:11:53 +0600

    geolocation hook created

--- src/hooks/useGeolocation.jsx
+++ src/hooks/useGeolocation.jsx
@@ -1,9 +1,25 @@
-const UseGeolocation = () => {
-    return (
-        <div>
+import {useState} from "react";
 
-        </div>
-    );
+const UseGeolocation = () => {
+    const [location,setLocation]=useState({});
+    const [error,setError] = useState(null);
+    const geoLocation = ()=>{
+        if (!navigator.geolocation){
+            return setError("Your browser does not support geolocation");
+        }
+        navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                setLocation({
+                    latitude:position.coords.latitude,
+                    longitude:position.coords.longitude
+                })
+            },
+            (err)=>{
+                setError(err.message);
+            }
+        )
+    }
+    return {location,error,geoLocation};
 };
 
 export default UseGeolocation;
